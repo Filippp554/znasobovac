aca6b19b358e3f2ecb6ce85b5e6aec9123a97945
Filippp554 <91588605+Filippp554@users.noreply.github.com>
2022-01-12 08:45:40 +0100

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>;
+using namespace std;
+
+int main() {
+
+	cout << "Napis cislo" << endl;
+	int cislo;
+	cin >> cislo;
+
+	int vysledok;
+	vysledok = 2 * cislo;
+
+	cout << "vysledok je " << vysledok << endl;
+	system("pause>0");
+
+}
